956d3bbc0730c08e7666b8fc47eea4b1bf5f4ee5
korene korologos <korenekorologos@gmail.com>
2020-01-23 10:24:29 -0700

    survey.html and apiRoutes.js have been updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -32,15 +32,39 @@ module.exports = function (app) {
 
         console.log("Name: " + userName);
         console.log("User Score " + userScores);
-    
+
         var sum = b.reduce((a, b) => a + b, 0);
-    
+
         console.log("Sum of users score " + sum);
         console.log("Best match friend diff " + bestMatch.friendDifference);
         console.log("+++++++=================++++++++++");
 
-//forloop that will loop through all of the friends possibilities in the database 
+        //forloop that will loop through all of the friends possibilities in the database 
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
+            totalDifference = 0;
+            console.log("Total Diff " + totalDifference);
+            console.log("Best match friend diff " + bestMatch.friendDifference);
 
+            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
+            console.log("Total friend score " + bfriendScore);
+            totalDifference += Math.abs(sum - bfriendScore);
+            console.log("-------------------------> " + totalDifference);
+
+            //if statement with sum the differences and sum the best match for the friend
+            if (totalDifference <= bestMatch.friendDifference) {
+                bestMatch.name = friends[i].name;
+                bestMatch.photo = friends[i].photo;
+                bestMatch.friendDifference = totalDifference;
+            }
+            console.log(totalDifference + " Total Difference");
+        }
+        console.log(bestMatch);
+
+        friends.push(userData);
+        console.log("New user added");
+        console.log(userData);
+        res.json(bestMatch); //returns a json with the user best match 
 
     });
 };
